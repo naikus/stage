ba81a8bc3e825592c124045d8146e699f0201efa
Aniket Naik <aniketn3@gmail.com>
2018-08-14 01:36:11 +0530

    Added back currentView and previousView until a resonable solution is found

--- src/stage.js
+++ src/stage.js
@@ -824,16 +824,11 @@
         popView: function(options) {
           return stage.popView(options);
         },
-        /*
         currentView: function() {
           return stage.currentView();
         },
         previousView: function() {
           return stage.previousView();
-        }
-        */
-        indexOfView: function(id) {
-          return stage.indexOfView(id)
         }
       };
     }
@@ -1232,7 +1227,6 @@
 
           popViewInternal(viewOptions, toViewId);
         },
-        /*
         currentView: function() {
           var currView = viewStack[viewStack.length - 1];
           return currView ? currView.id : null;
@@ -1245,7 +1239,6 @@
           }
           return null;
         },
-        */
         indexOfView: function(viewId) {
           var index = -1;
           viewStack.some(function(v, i) {
