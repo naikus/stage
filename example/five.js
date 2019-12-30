ba406b720b243f7628a1e45009b1907a9aba29ad
Aniket Naik <aniketn3@gmail.com>
2019-12-30 13:35:51 +0530

    Added fancy button transition

--- example/five.js
+++ example/five.js
@@ -3,14 +3,25 @@ Stage.defineView({
   template: `<div class="stage-view five">
     <h1>View Five</h1>
     <p id="dt"></p>
+    <button class="btn" id="btn8">&#171;</button>
+    <button class="btn" id="btn9">&#187;</button>
   </div>`,
   factory: function(stageContext, viewUi) {
     var dt;
     return {
       initialize: function() {
-        viewUi.addEventListener("click", function() {
+        viewUi.addEventListener(ACTION, function() {
           stageContext.popView({toView: "one"});
         });
+
+        document.getElementById("btn8").addEventListener(ACTION, function () {
+          stageContext.popView();
+        }, false);
+
+        document.getElementById("btn9").addEventListener(ACTION, function () {
+          stageContext.popView({toView: "one"});
+        }, false);
+
         dt = document.getElementById("dt");
         dt.innerHTML = Date();
       },
--- example/four.js
+++ example/four.js
@@ -1,3 +1,26 @@
-function dummy() {
-  console.log("Hello from view four remote script");
+function heroTransition(stageContext, viewUi) {
+  var hero = viewUi.getElementsByClassName("hero")[0],
+      onClick = function(e) {
+        hero.className = "hero transition"
+        viewUi.classList.add("hero-transitioning")
+      },
+      onTransitionEnd = function(e) {
+        stageContext.pushView("five", {
+          transition: "fade"
+        });
+      };
+  return {
+    init: function() {
+      hero.addEventListener("click", onClick);
+      hero.addEventListener("transitionend", onTransitionEnd);
+    },
+    reset: function() {
+      hero.className = "hero";
+      viewUi.classList.remove("hero-transitioning")
+    },
+    destroy: function() {
+      hero.removeEventListener("click", onClick);
+      hero.removeEventListener("transitionend", onTransitionEnd);
+    }
+  };
 }
